5f39d1f723992d63216216863b2bc293862f419a
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2023-12-17 20:56:57 -0300

    feat: create router, add ping route

---
+++ backend/src/server.ts
@@ -0,0 +1,33 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import http from "http";
+import https from "https";
+import publicRoutes from "./routes/public";
+import { requestInterceptor } from "./utils/requestInterceptor";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.all("*", requestInterceptor);
+
+app.use("/", publicRoutes);
+
+const runServer = (port: number, server: http.Server) => {
+  server.listen(port, () => {
+    console.log(`🚀 Server running on port ${port}`);
+  });
+};
+
+const regularServer = http.createServer(app);
+if (process.env.NODE_ENV === "production") {
+  // TODO: config SSL
+  // TODO: run server on port 80 and port 443
+} else {
+  const serverPort: number = process.env.PORT
+    ? parseInt(process.env.PORT)
+    : 9000;
+  runServer(serverPort, regularServer);
+}
---
+++ backend/src/utils/requestInterceptor.ts
@@ -0,0 +1,11 @@
+import { RequestHandler } from "express";
+
+export const requestInterceptor: RequestHandler = (req, res, next) => {
+  const body = JSON.stringify(req.body);
+
+  console.log(
+    `➡️   ${res.statusCode} ${req.method} ${req.originalUrl} ${body}`
+  );
+
+  next();
+};
